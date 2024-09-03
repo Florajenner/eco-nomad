d0d43494cff37e72d6df313eac17b4b371cd2f74
Flora Jenner <flora.jenner@gmail.com>
2024-09-03 23:30:42 +0000

    add trusted origins

--- eco_nomad/settings.py
+++ eco_nomad/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f3zbzox2f!2opo86h$)m$5kmzcwaib0$71@!bwh9n$4(&$9=e2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['8000-florajenner-economad-61z59698c1a.ws-eu115.gitpod.io',
@@ -91,6 +91,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
