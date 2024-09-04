0b173db3f46aaf555df88e5daf8f83b5c0759158
Flora Jenner <flora.jenner@gmail.com>
2024-09-04 15:46:20 +0000

    install summernote

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
-    template_name = 'blog/post_list.html'  # Explicitly specify the template
-    context_object_name = 'object_list'  # Explicitly specif
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
+    
