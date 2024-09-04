18e990b6f70decc214fa719b21375db70bb7dd36
Flora Jenner <flora.jenner@gmail.com>
2024-09-04 12:44:10 +0000

    add class based view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
 
-
-
-def index(request):
-    return HttpResponse("Hello, Blog!")
